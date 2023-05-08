e1b7b54cd2ecb05500f2f92605e1149a3a3025b6
pushkalkishore <kpushkal122@gmail.com>
2023-05-08 15:34:45 +0530

    Added the background to the Homepage

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="text-center">
-      <h1>Home page</h1>
+      <Image
+        src="/public/next.svg"
+        alt="Next.js Logo"
+        width={500}
+        height={500}
+      ></Image>
       <a href="./register">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
           Register Page
         </button>
       </a>
